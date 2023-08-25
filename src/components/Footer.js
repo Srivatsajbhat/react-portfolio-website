32d442355ec48b9f193c9e75877471efcfc0fafe
Srivatsa j bhat <94160935+Srivatsajbhat@users.noreply.github.com>
2023-08-25 08:19:27 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/Srivatsajbhat/"
+                href="https://www.linkedin.com/in/srivatsa-j-bhat-8289a9253"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
